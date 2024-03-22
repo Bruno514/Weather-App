58a078e32a2e35572df93538752a38fde25b8817
Bruno Oliveira <86791361+Bruno514@users.noreply.github.com>
2024-03-22 13:40:20 -0300

    finish autocomplete

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const APIKEY = "83e5b2e29e34a6651b74d699800634e6";
+const APIKEY_OW = "83e5b2e29e34a6651b74d699800634e6";
+const APIKEY_GEOAPIFY = "46472531fa224e08bcb7ed4ccf2eb86b";
 
-function getLatLon(limit) {
+function getLatLon(limit, input) {
   return new Promise((resolve, reject) => {
-    const location = document.querySelector("#location").value;
+    const location = input;
+    console.log(input)
 
     let request = fetch(
-      `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${limit}&appid=${APIKEY}`,
+      `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${limit}&appid=${APIKEY_OW}`,
       { mode: "cors" }
     );
 
@@ -15,6 +17,7 @@ function getLatLon(limit) {
           let err = new Error("HTTP status code: " + response.status);
           err.response = response;
           err.status = response.status;
+
           reject(err);
         }
 
@@ -32,16 +35,29 @@ function getLatLon(limit) {
   });
 }
 
-async function getWeatherData() {
+async function getAutocompleteCities(query) {
+  URL = `https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&apiKey=${APIKEY_GEOAPIFY}`;
+
+  try {
+    response = await fetch(URL, { mode: "cors" });
+    responseJson = await response.json();
+  } catch (err) {
+    throw err;
+  }
+
+  return responseJson;
+}
+
+async function getWeatherData(input) {
   const lang = document.querySelector("select").value.split(" ")[1];
 
   try {
-    let data = await getLatLon(1);
+    let data = await getLatLon(1, input);
 
     const lat = data[0].lat;
     const lon = data[0].lon;
 
-    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=${lang}&appid=${APIKEY}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=${lang}&appid=${APIKEY_OW}`;
 
     try {
       let request = await fetch(URL, { mode: "cors" });
@@ -65,11 +81,7 @@ function getDateString() {
   return `${dayLong}, ${monthLong} ${day}`;
 }
 
-// Event setup
-const locationForm = document.querySelector("form");
-locationForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
+async function displayWeatherData(input) {
   // Hide info display for now
   document.querySelector(".info-display").classList.remove("visible");
 
@@ -77,8 +89,7 @@ locationForm.addEventListener("submit", async (event) => {
   document.querySelector(".loader").classList.add("visible");
 
   try {
-    const data = await getWeatherData();
-    console.log(data);
+    const data = await getWeatherData(input);
 
     const temperatureConverted = Math.round(data.main.temp - 273.15);
     const dateString = getDateString();
@@ -106,29 +117,57 @@ locationForm.addEventListener("submit", async (event) => {
       description.charAt(0).toUpperCase() + description.slice(1)
     }`; // Capitalize
     document.querySelector(".city-name").textContent = `${city}, ${country}`; // Capitalize
+
+    document.querySelector(".autocomplete-items").classList.remove("visible");
   } catch (error) {
     console.error(error);
   }
+}
+
+// Event setup
+const locationForm = document.querySelector("form");
+locationForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  displayWeatherData();
 });
 
 // Some autocompletion
 const input = document.querySelector("input#location");
 input.addEventListener("keyup", async (event) => {
   try {
-    console.log(1)
-    const cities = await getLatLon(15);
+    const cities = await getAutocompleteCities(event.target.value);
 
-    const datalist = document.querySelector("datalist#cities");
-    datalist.innerHTML = "";
+    const autocompleteItems = document.querySelector(".autocomplete-items");
+    autocompleteItems.innerHTML = "";
 
-    cities.forEach((city) => {
-      const option = document.createElement("option");
-      option.value = city.name;
+    cities.features.forEach((feature) => {
+      const option = document.createElement("div");
+      const properties = feature.properties;
 
-      datalist.appendChild(option);
+      if (properties.hasOwnProperty("city")) {
+        const city = properties.city;
+
+        option.textContent = city;
+
+        option.addEventListener("click", function () {
+          displayWeatherData(city);
+        });
+
+        autocompleteItems.appendChild(option);
+      }
     });
-  } 
-  catch (err) {
+  } catch (err) {
     console.error(err);
   }
+});
+
+// Show and hive autocomplete
+input.addEventListener("focus", () => {
+  document.querySelector(".autocomplete-items").classList.add("visible");
+});
+input.addEventListener("focusout", () => {
+  setTimeout(() => {
+    document.querySelector(".autocomplete-items").classList.remove("visible");
+  }, 200);
 });
